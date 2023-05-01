8890e0cf34c8842f36bf5718e9a04c48ca54e13d
Marcelo Almeida <marcelo_almeida@live.com>
2023-05-01 16:57:44 -0300

    Revisão Final

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# astro-mvp-frontend
+# Corpos Celestes
+
+Projeto sucinto em javascript puro que lista objetos astronômicos cadastrados e nos permite fazer nova inclusão e deleção de itens já cadastrados.
+
+Objetivo é conhecer os fundamentos do desenvolvimento web evitando utilizar frameworks complexos, focando no aprendizado em JS, CSS e HTML.
+
+## Requirements
+
+Navegador com suporte ao ECMAScript 13.
+
+## Installation
+
+Clone o repositório para sua máquina e abra o arquivo index.html utilizando o navegador de sua preferência.
+
+## Usage
+
+Utilize a barra lateral para fornecer dados do corpo celeste e em seguida adicione-o para que seja exibido na lista à direita.
+
+## Contributing
+
+Projeto faz parte do MVP do curso de Engenharia de Software da PUC-Rio e se encontra finalizado, não aceitando novas edições.
+
+## License
+
+This project is licensed under the [License Name] - see the [LICENSE](LICENSE) file for details.
--- script.js
+++ script.js
@@ -5,51 +5,6 @@ async function getList() {
     addItem(jsonData)
 }
 
-function addItem(astro) {
-    astro.forEach(item => {
-        // Get reference to the unordered list element
-        const myList = document.getElementById("list");
-
-        // Create a new list item element
-        const newItem = document.createElement("li");
-        newItem.id = item.id
-
-        // Create 3 span elements with classes
-        const spanName = document.createElement("span");
-        spanName.classList.add("stellar-body-name");
-        spanName.textContent = item.name
-
-        const spanDistance = document.createElement("span");
-        spanDistance.classList.add("stellar-body-distance");
-        spanDistance.textContent = item.distance
-
-        const spanType = document.createElement("span");
-        spanType.classList.add("stellar-body-type");
-        spanType.textContent = item.type
-
-        // Create a div element containing 2 buttons
-        const div = document.createElement("div");
-
-        const button2 = document.createElement("button");
-        button2.classList.add("delete-btn");
-        button2.textContent = "Delete";
-        button2.addEventListener("click", deleteAstro)
-        div.appendChild(button2);
-
-        // Append the spans and div to the new list item
-        newItem.appendChild(spanName);
-        newItem.appendChild(spanDistance);
-        newItem.appendChild(spanType);
-        newItem.appendChild(div);
-
-        // Append the new list item to the unordered list
-        myList.appendChild(newItem);
-    });
-
-
-
-}
-
 function postAstro() {
     const name = document.getElementById("name").value;
     const distance = document.getElementById("distance").value;
@@ -78,4 +33,49 @@ async function deleteAstro(event) {
         method: "DELETE"
     }).then(response => response.json())
         .catch(error => console.error(error)).then(() => window.location.reload());
+}
+
+function addItem(astro) {
+    astro.forEach(item => {
+        // Get reference to the unordered list element
+        const myList = document.getElementById("list");
+
+        // Create a new list item element
+        const newItem = document.createElement("li");
+        newItem.id = item.id
+
+        // Create 3 span elements with classes
+        const spanName = document.createElement("span");
+        spanName.classList.add("stellar-body-name");
+        spanName.textContent = item.name
+
+        const spanDistance = document.createElement("span");
+        spanDistance.classList.add("stellar-body-distance");
+        spanDistance.textContent = item.distance + " AU"
+
+        const spanType = document.createElement("span");
+        spanType.classList.add("stellar-body-type");
+        spanType.textContent = item.type
+
+        // Create a div element containing 2 buttons
+        const div = document.createElement("div");
+
+        const button2 = document.createElement("button");
+        button2.classList.add("delete-btn");
+        button2.textContent = "Delete";
+        button2.addEventListener("click", deleteAstro)
+        div.appendChild(button2);
+
+        // Append the spans and div to the new list item
+        newItem.appendChild(spanName);
+        newItem.appendChild(spanDistance);
+        newItem.appendChild(spanType);
+        newItem.appendChild(div);
+
+        // Append the new list item to the unordered list
+        myList.appendChild(newItem);
+    });
+
+
+
 }
